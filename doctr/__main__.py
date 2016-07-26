ef346fbbf03bf599cad8f702442fe811ceb41718
Gil Forsyth <gilforsyth@gmail.com>
2016-07-26 13:39:44 -0400

    remove note about creating gh-pages branch (automated)

--- doctr/__main__.py
+++ doctr/__main__.py
@@ -63,9 +63,7 @@ env:
 
 """.format(encrypted_variable=encrypted_variable.decode('utf-8'))
 
-        print("Put\n", travis_content, "in your .travis.yml.\n",
-            "Also make sure to create an empty gh-pages branch on GitHub, and "
-            "enable it at https://github.com/{repo}/settings".format(repo=repo), sep='')
+        print("Put\n", travis_content, "in your .travis.yml.\n")
 
 if __name__ == '__main__':
     sys.exit(main())
