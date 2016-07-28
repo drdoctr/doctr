6efbe921423488eb339d1c7d5d0e228a2116a35e
Aaron Meurer <asmeurer@gmail.com>
2016-07-28 18:20:17 -0400

    Split doctr into explicit 'doctr local' and 'doctr travis' subcommands
    It requires --force if travis (local) is run and it detects that it is (not)
    on Travis.
    
    I'm undecided if I actually want this behavior or not. The behavior before
    this detects it automatically, allowing the user to switch the behavior if
    so-desired. This way is more explicit.
    
    In any case, it's worth noting that running the wrong command in the wrong
    place almost never is what you want (except for us developers doing testing).
    The local command requires user input. The Travis command requires the
    encrypted GH_TOKEN variable, and also makes some assumptions that are always
    true on Travis (like that it is on Linux, that it will be run from the git
    repo directory, and that the git remote is https).

--- doctr/__main__.py
+++ doctr/__main__.py
@@ -5,7 +5,7 @@ A tool to automatically deploy docs to GitHub pages from Travis CI.
 
 The doctr command is two commands in one. To use, first run
 
-doctr
+doctr local
 
 on your local machine. This will prompt for your GitHub credentials and the
 name of the repo you want to deploy docs for. This will generate a secure key,
@@ -13,7 +13,7 @@ which you should insert into your .travis.yml.
 
 Then, on Travis, for the build where you build your docs, add
 
-    - doctr
+    - doctr travis
 
 to the end of the build to deploy the docs to GitHub pages.  This will only
 run on the master branch, and won't run on pull requests.
@@ -30,42 +30,64 @@ from .travis import setup_GitHub_push, commit_docs, push_docs, get_repo
 from . import __version__
 
 def main():
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter, epilog="""
+Run --help on the subcommands like 'doctr travis --help' to see the
+options available.
+        """,
+        )
     parser.add_argument('-V', '--version', action='version', version='doctr ' + __version__)
-    location = parser.add_mutually_exclusive_group()
-    location.add_argument('--travis', action='store_true', default=None,
-    help="Run as if on Travis. The default is to detect automatically.")
-    location.add_argument('--local', action='store_true', default=None,
-    help="Run as if local (not on Travis). The default is to detect automatically.")
+
+    location = parser.add_subparsers(title='location', dest='location',
+        description="Location doctr is being run from")
+    travis_parser = location.add_parser('travis', help="Run as if on Travis.")
+    travis_parser.set_defaults(func=travis)
+    travis_parser.add_argument('--force', action='store_true', help="""Run the travis command even
+    if we do not appear to be on Travis.""")
+
+    local_parser = location.add_parser('local', help="Run as if local (not on Travis).")
+    local_parser.set_defaults(func=local)
+    local_parser.add_argument('--force', action='store_true', help="""Run the local command even
+    if we appear to be on Travis.""")
 
     args = parser.parse_args()
 
-    if args.local == args.travis == None:
-        on_travis = os.environ.get("TRAVIS_JOB_NUMBER", '')
-    else:
-        on_travis = args.travis
+    args.func(args, parser)
 
-    if on_travis:
-        repo = get_repo()
-        if setup_GitHub_push(repo):
-            commit_docs()
-            push_docs()
-    else:
-        username = input("What is your GitHub username? ")
-        token = generate_GitHub_token(username)
 
-        repo = input("What repo to you want to build the docs for? ")
-        encrypted_variable = encrypt_variable("GH_TOKEN={token}".format(token=token).encode('utf-8'), repo=repo)
-        travis_content = """
+def on_travis():
+    return os.environ.get("TRAVIS_JOB_NUMBER", '')
+
+def travis(args, parser):
+    if not args.force and not on_travis():
+        parser.error("doctr does not appear to be running on Travis. Use "
+            "doctr travis --force to run anyway.")
+
+    repo = get_repo()
+    if setup_GitHub_push(repo):
+        commit_docs()
+        push_docs()
+
+def local(args, parser):
+    if not args.force and on_travis():
+        parser.error("doctr appears to be running on Travis. Use "
+            "doctr local --force to run anyway.")
+
+    username = input("What is your GitHub username? ")
+    token = generate_GitHub_token(username)
+
+    repo = input("What repo to you want to build the docs for? ")
+    encrypted_variable = encrypt_variable("GH_TOKEN={token}".format(token=token).encode('utf-8'), repo=repo)
+    travis_content = """
 env:
   global:
     secure: "{encrypted_variable}"
 
 """.format(encrypted_variable=encrypted_variable.decode('utf-8'))
 
-        print("Put\n", travis_content, "in your .travis.yml.\n",
-            "Also make sure to create an empty gh-pages branch on GitHub, and "
-            "enable it at https://github.com/{repo}/settings".format(repo=repo), sep='')
+    print("Put\n", travis_content, "in your .travis.yml.\n",
+        "Also make sure to create an empty gh-pages branch on GitHub, and "
+        "enable it at https://github.com/{repo}/settings".format(repo=repo), sep='')
 
 if __name__ == '__main__':
     sys.exit(main())
