3a735963ded30a3bfde352829d4b5fdf88b8062f
Gil Forsyth <gilforsyth@gmail.com>
2016-07-29 18:37:59 -0400

    I AM SO SMART, I AM SO SMART, S-M-R-T

--- doctr/__init__.py
+++ doctr/__init__.py
@@ -1,14 +1,14 @@
 from .local import encrypt_variable, generate_GitHub_token
 from .travis import (get_token, run, setup_GitHub_push, gh_pages_exists,
                      create_gh_pages, commit_docs, push_docs, get_repo,
-                     find_doc_dir)
+                     find_build_dir)
 
 __all__ = [
     'encrypt_variable', 'generate_GitHub_token',
 
     'get_token', 'run', 'setup_GitHub_push', 'gh_pages_exists',
     'create_gh_pages', 'commit_docs', 'push_docs', 'get_repo',
-    'find_doc_dir',
+    'find_build_dir',
 ]
 
 from ._version import get_versions
