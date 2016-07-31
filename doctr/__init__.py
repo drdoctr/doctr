45c334c9184c460db6b6fbb128260ca7ad15d3c1
Aaron Meurer <asmeurer@gmail.com>
2016-07-31 15:40:34 -0400

    Export generate_ssh_key

--- doctr/__init__.py
+++ doctr/__init__.py
@@ -1,9 +1,9 @@
-from .local import encrypt_variable, encrypt_file, generate_GitHub_token
+from .local import (encrypt_variable, encrypt_file, generate_GitHub_token, generate_ssh_key)
 from .travis import (decrypt_file, get_token, run, setup_GitHub_push,
     gh_pages_exists, create_gh_pages, commit_docs, push_docs, get_repo)
 
 __all__ = [
-    'encrypt_variable', 'encrypt_file', 'generate_GitHub_token',
+    'encrypt_variable', 'encrypt_file', 'generate_GitHub_token', 'generate_ssh_key',
 
     'decrypt_file', 'get_token', 'run', 'setup_GitHub_push', 'gh_pages_exists',
     'create_gh_pages', 'commit_docs', 'push_docs', 'get_repo',
