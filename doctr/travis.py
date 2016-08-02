8ee5f217bb9aed46c4e8e17581ae40ed6e360f32
Gil Forsyth <gilforsyth@gmail.com>
2016-08-02 15:43:45 -0400

    run ssh-agent at the same time as ssh-add

--- doctr/travis.py
+++ doctr/travis.py
@@ -62,8 +62,7 @@ def setup_deploy_key():
                 "  LogLevel ERROR\n" % key_path)
 
     # start ssh-agent and add key to it
-    run(['eval', 'ssh-agent -s'])
-    run(['ssh-add', os.path.expanduser('~/.ssh/github_deploy_key')])
+    run(['ssh-agent', 'ssh-add', os.path.expanduser('~/.ssh/github_deploy_key')])
 
 # XXX: Do this in a way that is streaming
 def run_command_hiding_token(args, token):
