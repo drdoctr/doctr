0b31d2a0d92961c1530cac70370186524786eea4
Aaron Meurer <asmeurer@gmail.com>
2016-08-04 22:40:31 -0400

    Make instructions more clear on steps the user needs to take

--- doctr/__main__.py
+++ doctr/__main__.py
@@ -95,6 +95,17 @@ def deploy(args, parser):
         commit_docs()
         push_docs()
 
+class IncrementingInt:
+    def __init__(self, i=0):
+        self.i = i
+
+    def __repr__(self):
+        ret = repr(self.i)
+        self.i += 1
+        return ret
+
+    __str__ = __repr__
+
 def configure(args, parser):
     if not args.force and on_travis():
         parser.error("doctr appears to be running on Travis. Use "
@@ -102,13 +113,18 @@ def configure(args, parser):
 
     repo = input("What repo to you want to build the docs for? ")
 
+    N = IncrementingInt(1)
+
     if args.token:
         token = generate_GitHub_token()
         encrypted_variable = encrypt_variable("GH_TOKEN={token}".format(token=token).encode('utf-8'),
         repo=repo)
-        print(dedent("""\
+        print(dedent("""
         A personal access token for doctr has been created.
-        You can go to https://github.com/settings/tokens to revoke it.\n"""))
+
+        You can go to https://github.com/settings/tokens to revoke it."""))
+
+        print("\n============ You should now do the following ============\n")
     else:
         ssh_key = generate_ssh_key("doctr deploy key for {repo}".format(repo=repo), keypath=args.key_path)
         key = encrypt_file(args.key_path, delete=True)
@@ -120,27 +136,28 @@ def configure(args, parser):
 
             upload_GitHub_deploy_key(repo, ssh_key)
 
-            print(dedent("""\
+            print(dedent("""
             The deploy key has been added for {repo}.
 
-            You can go to {deploy_keys_url} to revoke the deploy key.
+            You can go to {deploy_keys_url} to revoke the deploy key.\
             """.format(repo=repo, deploy_keys_url=deploy_keys_url, keypath=args.key_path)))
+            print("\n============ You should now do the following ============\n")
         else:
+            print("\n============ You should now do the following ============\n")
             print(dedent("""\
-            Go to {deploy_keys_url} and add the following as a new key:
+            {N}. Go to {deploy_keys_url} and add the following as a new key:
 
             {ssh_key}
-
             Be sure to allow write access for the key.
-            """.format(ssh_key=ssh_key, deploy_keys_url=deploy_keys_url)))
+            """.format(ssh_key=ssh_key, deploy_keys_url=deploy_keys_url, N=N)))
 
         # TODO: Should we just delete the public key?
 
         print(dedent("""\
-        Commit the file {keypath}.enc. The file {keypath}.pub contains the
+        {N}. Commit the file {keypath}.enc. The file {keypath}.pub contains the
         public deploy key for GitHub. It does not need to be
         committed.
-        """.format(keypath=args.key_path)))
+        """.format(keypath=args.key_path, N=N)))
 
         if args.key_path != 'github_deploy_key':
             options = '--key-path {keypath}.enc'.format(keypath=args.key_path)
@@ -148,22 +165,22 @@ def configure(args, parser):
             options = ''
 
         print(dedent("""\
-        Add
+        {N}. Add
 
             - doctr deploy {options}
 
         to the docs build of your .travis.yml.
-        """.format(options=options)))
+        """.format(options=options, N=N)))
 
     print(dedent("""\
-    Put
+    {N}. Put
 
         env:
           global:
             - secure: "{encrypted_variable}"
 
     in your .travis.yml.
-    """.format(encrypted_variable=encrypted_variable.decode('utf-8'))))
+    """.format(encrypted_variable=encrypted_variable.decode('utf-8'), N=N)))
 
 if __name__ == '__main__':
     sys.exit(main())
