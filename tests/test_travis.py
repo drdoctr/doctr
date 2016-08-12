6a4ec01d399dc7fe016f63706202324f050bbe44
Aaron Meurer <asmeurer@gmail.com>
2016-08-12 15:25:31 -0400

    Move test outside of the library
    It can't pass when the library is installed anyway (it tests stuff that isn't
    installed).

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -5,8 +5,8 @@ from doctr.travis import find_sphinx_build_dir
 def test_find_sphinx_build_dir():
     curdir = os.path.abspath(os.curdir)
     try:
-        os.chdir(os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir)))
-        assert os.path.basename(os.path.abspath(os.path.curdir)) == 'doctr'
+        os.chdir(os.path.abspath(os.path.join(__file__, os.pardir, os.pardir)))
+        assert 'tests' in os.listdir(os.curdir)
         assert find_sphinx_build_dir() == os.path.join('docs', '_build')
     finally:
         os.chdir(curdir)
