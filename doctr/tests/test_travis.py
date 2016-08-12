dbe8346aa406cacc8629cfa0b498fe228ee24d77
Aaron Meurer <asmeurer@gmail.com>
2016-08-12 15:21:46 -0400

    Add a simple test for find_sphinx_build_dir()
    Also, enable tests in Travis.

---
+++ doctr/tests/test_travis.py
@@ -0,0 +1,12 @@
+import os
+
+from doctr.travis import find_sphinx_build_dir
+
+def test_find_sphinx_build_dir():
+    curdir = os.path.abspath(os.curdir)
+    try:
+        os.chdir(os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir)))
+        assert os.path.basename(os.path.abspath(os.path.curdir)) == 'doctr'
+        assert find_sphinx_build_dir() == os.path.join('docs', '_build')
+    finally:
+        os.chdir(curdir)
