2aa291f0b286eca3bda39f507640e3d0b0873188
Gil Forsyth <gilforsyth@gmail.com>
2016-08-21 18:05:04 -0400

    update local tests for RuntimeError

--- doctr/tests/test_local.py
+++ doctr/tests/test_local.py
@@ -3,11 +3,11 @@ from ..local import check_repo_exists
 from pytest import raises
 
 def test_bad_user():
-    with raises(SystemExit):
+    with raises(RuntimeError):
         check_repo_exists('---/invaliduser')
 
 def test_bad_repo():
-    with raises(SystemExit):
+    with raises(RuntimeError):
         check_repo_exists('drdoctr/---')
 
 def test_repo_exists():
