aeca05f0f5260f176d06fea4e7ba823865815c99
Aaron Meurer <asmeurer@gmail.com>
2016-08-30 19:19:56 -0400

    check_output returns bytes

--- doctr/travis.py
+++ doctr/travis.py
@@ -164,7 +164,7 @@ def setup_GitHub_push(deploy_repo, auth_type='deploy_key', full_key_path='github
     # Should we add some user.email?
     run(['git', 'config', '--global', 'user.name', "Doctr (Travis CI)"])
 
-    remotes = subprocess.check_output(['git', 'remote']).split('\n')
+    remotes = subprocess.check_output(['git', 'remote']).decode('utf-8').split('\n')
     if 'doctr_remote' in remotes:
         print("doctr_remote already exists")
     else:
