78d1d726af98757765718536c17d6677011e6ef7
Aaron Meurer <asmeurer@gmail.com>
2017-07-15 16:56:19 -0500

    Use a git command to find the remote that works on older versions of git

--- doctr/local.py
+++ doctr/local.py
@@ -264,7 +264,7 @@ def guess_github_repo():
 
     Returns False if no guess can be made.
     """
-    p = subprocess.run(['git', 'remote', 'get-url', 'origin'],
+    p = subprocess.run(['git', 'ls-remote', '--get-url', 'origin'],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
     if p.stderr or p.returncode:
         print(p)
