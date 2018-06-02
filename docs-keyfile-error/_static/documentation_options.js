9ae9e1f282dfaee04e10dad65f551c44b5d11f2a
Travis CI User <travis@example.org>
2018-06-02 16:07:27 +0000

    Update docs after building Travis build 1159 of drdoctr/doctr
    The docs were built from the branch 'keyfile-error' against the commit
    acd688a973af507a2ee2ab994f9b974145f6ef51.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/387138213.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-keyfile-error --command 'echo test && ls && touch docs-keyfile-error/test-file && git add docs-keyfile-error/test-file'

--- docs-keyfile-error/_static/documentation_options.js
+++ docs-keyfile-error/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '1.7.1+30.gb3a5d14',
+    VERSION: '1.7.3+5.gacd688a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
