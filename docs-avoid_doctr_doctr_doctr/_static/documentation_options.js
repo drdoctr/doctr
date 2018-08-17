8287293831d6d9a14e7955ca4fcf82c0c9f35d69
Travis CI User <travis@example.org>
2018-08-17 22:41:49 +0000

    Update docs after building Travis build 1189 of drdoctr/doctr
    The docs were built from the branch 'avoid_doctr_doctr_doctr' against the commit
    f0ad32ab07b7af9c566760ddc9fd25a5bcffdd63.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/417474730.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-avoid_doctr_doctr_doctr --command 'echo test && ls && touch docs-avoid_doctr_doctr_doctr/test-file && git add docs-avoid_doctr_doctr_doctr/test-file'

--- docs-avoid_doctr_doctr_doctr/_static/documentation_options.js
+++ docs-avoid_doctr_doctr_doctr/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.3+19.gecbf395',
+    VERSION: '1.7.3+20.gf0ad32a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
