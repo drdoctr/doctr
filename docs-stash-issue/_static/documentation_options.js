b58ef8e073bb4bea1715f9767e394c25316d1827
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-09-05 23:22:17 +0000

    Update docs after building Travis build 1225 of drdoctr/doctr
    The docs were built from the branch 'stash-issue' against the commit
    ce5baf3cc965bb1403deec29e136878ce848a8dd.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/425037550.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-stash-issue --command 'echo test && ls && touch docs-stash-issue/test-file && git add docs-stash-issue/test-file'

--- docs-stash-issue/_static/documentation_options.js
+++ docs-stash-issue/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.3+29.g333d6f0',
+    VERSION: '1.7.3+30.gce5baf3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
