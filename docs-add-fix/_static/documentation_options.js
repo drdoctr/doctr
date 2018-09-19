4dba88da54b63b425d94c5bc4ceb0d4712f1b760
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-09-19 04:07:00 +0000

    Update docs after building Travis build 1237 of drdoctr/doctr
    The docs were built from the branch 'add-fix' against the commit
    b6628cea60738057f410a78df6f396ceed8bd30c.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/430366895.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-add-fix --command 'echo test && ls && touch docs-add-fix/test-file && git add docs-add-fix/test-file'

--- docs-add-fix/_static/documentation_options.js
+++ docs-add-fix/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.3+35.gaef1112',
+    VERSION: '1.7.3+36.gb6628ce',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
