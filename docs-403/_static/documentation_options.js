e7cb4b19ab24887b0548ae17b046f7283c17121b
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-09-19 17:13:44 +0000

    Update docs after building Travis build 1242 of drdoctr/doctr
    The docs were built from the branch '403' against the commit
    cc7ece0840cd24c39d2388737fd8bf5336cc7f03.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/430649067.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-403 --command 'echo test && ls && touch docs-403/test-file && git add docs-403/test-file'

--- docs-403/_static/documentation_options.js
+++ docs-403/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.3+42.g1ecec1e',
+    VERSION: '1.7.3+43.gcc7ece0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
