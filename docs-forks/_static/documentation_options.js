c975e89ae0ad7ff0036f846fb1058cd39a6259fa
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-11-27 22:16:54 +0000

    Update docs after building Travis build 1287 of drdoctr/doctr
    The docs were built from the branch 'forks' against the commit
    14e49f7cacd236fe4677da8413741e150b502726.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/460493359.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-forks --command 'echo test && ls && touch docs-forks/test-file && git add docs-forks/test-file'

--- docs-forks/_static/documentation_options.js
+++ docs-forks/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.4+33.g282f92f',
+    VERSION: '1.7.4+34.g14e49f7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
