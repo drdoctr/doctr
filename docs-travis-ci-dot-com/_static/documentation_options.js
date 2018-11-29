c6189908762b913e255d8dd7cec3a8c5e646c412
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-11-29 21:09:38 +0000

    Update docs after building Travis build 1311 of drdoctr/doctr
    The docs were built from the branch 'travis-ci-dot-com' against the commit
    a6b8cfc9d950b61ff56e0ed7b4e44157a44cc6b2.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/461481507.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-travis-ci-dot-com --command 'echo test && ls && touch docs-travis-ci-dot-com/test-file && git add docs-travis-ci-dot-com/test-file'

--- docs-travis-ci-dot-com/_static/documentation_options.js
+++ docs-travis-ci-dot-com/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.4+37.g4da7d99',
+    VERSION: '1.7.4+43.ga6b8cfc',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
