fab3d37d1fbeb4b2a93605e00364383b8352a710
Aaron Meurer <asmeurer@gmail.com>
2019-01-31 16:33:41 -0700

    Don't print about generating a token when one isn't generated

--- doctr/local.py
+++ doctr/local.py
@@ -245,10 +245,9 @@ def get_travis_token(*, GitHub_token=None, **login_kwargs):
     headersv2 = {**_headers, **Travis_APIv2}
     token_id = None
     try:
-        print(green("I need to generate a temporary token with GitHub to authenticate with Travis. You may get a warning email from GitHub about this."))
-        print(green("It will be deleted immediately. If you still see it after this at https://github.com/settings/tokens after please delete it manually."))
-
         if not GitHub_token:
+            print(green("I need to generate a temporary token with GitHub to authenticate with Travis. You may get a warning email from GitHub about this."))
+            print(green("It will be deleted immediately. If you still see it after this at https://github.com/settings/tokens after please delete it manually."))
             # /auth/github doesn't seem to exist in the Travis API v3.
             tok_dict = generate_GitHub_token(scopes=["read:org", "user:email", "repo"],
                                              note="temporary token for doctr to auth against travis (delete me)",
