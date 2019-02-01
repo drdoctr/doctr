2d5877c62b07bc3f94b2bb8636bd70cc76891fbe
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-01 19:37:18 +0000

    Update docs after building Travis build 1351 of drdoctr/doctr
    The docs were built from the branch 'error-messages' against the commit
    1772f53f7ad30234158f7f32bff16e4867a4dab0.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/487610784.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-error-messages --command 'echo test && ls && touch docs-error-messages/test-file && git add docs-error-messages/test-file'

--- docs-error-messages/_static/documentation_options.js
+++ docs-error-messages/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.4+79.g8c16e77',
+    VERSION: '1.7.4+82.g1772f53',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
