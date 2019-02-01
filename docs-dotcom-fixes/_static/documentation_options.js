448c8777bfa7d4909165d4f8e7c45750220c0669
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-01 18:49:26 +0000

    Update docs after building Travis build 1344 of drdoctr/doctr
    The docs were built from the branch 'dotcom-fixes' against the commit
    8c97dbe16cca696a4923bbc7ebcb4248a60c6aab.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/487592950.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-dotcom-fixes --command 'echo test && ls && touch docs-dotcom-fixes/test-file && git add docs-dotcom-fixes/test-file'

--- docs-dotcom-fixes/_static/documentation_options.js
+++ docs-dotcom-fixes/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.4+83.g84d9630',
+    VERSION: '1.7.4+81.g8c97dbe',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
