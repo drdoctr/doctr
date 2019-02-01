af3c05dfa1c2c42bc1668d4a98e9f28bb9db4f23
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-01 20:53:43 +0000

    Update docs after building Travis build 1367 of drdoctr/doctr
    The docs were built from the branch 'python37' against the commit
    ab35d8684e413d58c0ba80cb08d1832344547879.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/487640561.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --built-docs docs/_build/html docs-python37 --command 'echo test && ls && touch docs-python37/test-file && git add docs-python37/test-file'

--- docs-python37/_static/documentation_options.js
+++ docs-python37/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.0+2.gedb0179',
+    VERSION: '1.8.0+3.gab35d86',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
