1b89f8a6f90f7e31c1905085d3c47adf21ff1ee8
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-11-20 00:40:48 +0000

    Update docs after building Travis build 1410 of drdoctr/doctr
    The docs were built from the branch 'github-auth' against the commit
    cd89c8bbcfdf4adb146b9e9f149887ec84b622fd.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/drdoctr/doctr/jobs/744767924.
    
    The doctr command that was run is
    
        /home/travis/build/drdoctr/doctr/doctr/__main__.py deploy --sync --key-path deploy_key.enc --no-require-master --deploy-branch gh-pages-testing docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.8.0+10.g1e5be24',
+    VERSION: '1.8.0+11.gcd89c8b',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
